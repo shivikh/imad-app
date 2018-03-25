595aeb9a3d937c163eb3bfa5c01c055934609dfe
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-03-25 13:09:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(createTemplate(articleOne));
+    res.send(createTemplate(articleOne));
 });
 
 /*app.get('/article-two', function (req, res) {
