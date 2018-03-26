3bfcb3439696b68b24ae0f86a61812fe98d53f02
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-03-26 10:42:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
+
 //change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move the image
+var img=document.getElementById['nodi'];
+img.onclick=function(){
+    img.style.marginLeft='100px';
+}
