ba64aecd84ccb319122fb433aa21b08cef3dd4bc
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-03-26 10:54:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,11 @@ element.innerHTML='new value';
 
 //move the image
 var img=document.getElementById('nodi');
+function moveRight()
+{
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
-  
-};
+    var interval=setInterval(moveRight,50);
+ };
