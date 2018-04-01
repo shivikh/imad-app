cfbbf7af1a79f576cafc0a64a9b291f6f486b784
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-04-01 16:38:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -61,7 +61,7 @@ submit.onClick = function()
     var list = '';
     for(var i=0;i<name.length;i++)
     {
-        list+='<li>' + name[i] + '</li>';
+        list += '<li>' + name[i] + '</li>';
     }
     var ul = getElementById('namelist');
     ul.innerHTML = list;
