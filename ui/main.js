9b923371dcb48fc855272fa9da901f6e82ce6144
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-04-01 16:03:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,11 +20,33 @@ img.onclick=function(){
 //counter end point
 
 
-var button=document.getElementById('counter');
+/*var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
+};*/
+
+//counter 
+
+var button=document.getElementById('counter');
+button.onclick=function()
+{
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readystate===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://coco98.imad.hasura.io/counter',true);
+    request.send(null);
 };
