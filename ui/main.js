ee9aa2ecde297aeec08a9e5ae33ad29205840391
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-04-01 16:07:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,19 +31,19 @@ button.onclick=function()
 
 //counter 
 
-var button=document.getElementById('counter');
-button.onclick=function()
+var button = document.getElementById('counter');
+button.onclick = function()
 {
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function()
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
     {
-        if(request.readyState===XMLHttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE)
         {
-            if(request.status===200)
+            if(request.status === 200)
             {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
             }
         }
     };
