090b0fb51fe3c6535c187ca648d16e2d91e46a55
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-04-05 21:41:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config{
+    user:'skhandelwal58821',
+    database:'skhandelwal58821',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -69,8 +78,27 @@ var htmlTemplate=`
 `;
 return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var Pool = new Pool(config);
+app.get('/text-db',function(req,res)
+{
+   //make a select request
+   //return a response with the result
+   Pool.query('SELECT * FROM test',function(err,result)
+   {
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send(JSON,stringify(result));
+       }
+   })
 });
 
 var counter=0;
