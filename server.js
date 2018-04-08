ab6127e6a932983db43058699ebf728629d7ecb1
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-04-08 20:00:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user:'skhandelwal58821',
@@ -81,6 +82,18 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt)
+{
+    var hashed = crypto.pbkdf25yn(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res)
+{
+   var hashedString = hash(req.params.input, 'this-is-some-string');
+   res.send(hashedStrinng);
 });
 
 var pool = new Pool(config);
