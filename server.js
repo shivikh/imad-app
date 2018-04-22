57605f5361309fa54be281e3090c7b5c1c16467e
Shivani Khandelwal <34422050+shivikh@users.noreply.github.com>
2018-04-22 19:35:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -189,8 +189,7 @@ app.get('/submit_name',function(req,res)
 
 
 app.get('/articles/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title='" + req.params.articleName + "'",function(req,res)
+    pool.query("SELECT * FROM article WHERE title='" + req.params.articleName + "'",function(err,result)
     {
        if(err)
        {
